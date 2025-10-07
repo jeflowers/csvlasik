fea84e0b76242ef026490f6d41100c8ed682aae1
John Flowers <jeflowers@gmail.com>
2025-10-07 14:53:23 -0500

    Deleted compress_lasik.sh

--- public/assets/videos/compress_lasik.sh
+++
@@ -1,9 +0,0 @@
-ffmpeg -i "Do-I-Need-LASIK.mp4" \
-  -c:v libx264 \
-  -crf 24 \
-  -preset slow \
-  -vf "scale=1920:-2" \
-  -c:a aac \
-  -b:a 128k \
-  -movflags +faststart \
-  "Do-I-Need-LASIK_optimized.mp4"
