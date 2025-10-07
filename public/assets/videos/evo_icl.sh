6a8c1b1053578debc2e37a4cb46062fb4433f3d3
John Flowers <jeflowers@gmail.com>
2025-10-07 14:53:50 -0500

    Deleted evo_icl.sh

--- public/assets/videos/evo_icl.sh
+++
@@ -1,9 +0,0 @@
-ffmpeg -ss 00:00:03 \
-  -i "EVO-Visian-ICL-Procedure-Animation.mp4" \
-  -c:v libx264 \
-  -crf 23 \
-  -preset medium \
-  -c:a aac \
-  -b:a 128k \
-  -movflags +faststart \
-  "EVO-Visian-ICL-Procedure-Animation_trimmed.mp4"
