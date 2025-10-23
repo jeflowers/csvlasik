e242efbe4329688c13700b2da24c8da561a5fd67
John Flowers <jeflowers@gmail.com>
2025-10-23 02:56:35 -0500

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -104,7 +104,7 @@ const Footer = () => {
             <h3 className="text-lg font-serif font-light mb-6 text-white tracking-wide">{t('contact.title')}</h3>
             <div className="space-y-5">
               <a
-                href="https://www.google.com/maps/dir/?api=1&destination=Clearsight+LASIK+Guam,+230+Archbishop+Flores+Street,+Suite+201,+Hagåtña,+Guam+96910"
+                href="https://www.google.com/maps/place/5750+Downey+Ave+suite+101,+Lakewood,+CA+90712/@33.8595678,-118.1533687,17z/data=!3m1!4b1!4m6!3m5!1s0x80dd32e42e2695f1:0x1108f8a7ff2c6497!8m2!3d33.8595678!4d-118.1507884!16s%2Fg%2F11ygln9y7v?entry=ttu&g_ep=EgoyMDI1MTAyMC4wIKXMDSoASAFQAw%3D%3D"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-start space-x-4 hover:opacity-80 transition-opacity duration-300"
